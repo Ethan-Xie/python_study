0e251709de37d38e3ea9af2202d8f94766d3a64f
xiethan <xiethan@qq.com>
2018-05-16 19:21:49 +0800

    对象学习
    5.16

--- object.py
+++ object.py
@@ -2,7 +2,7 @@
 # 面向对象特性：封装，继承，多态。类、方法
 
 # 重复的代码是非常不好的低级行为，你写的代码要经常变更（易读，易改）
-
+""" 对象示例
 class Role(object):
     def __init__(self, name, role, weapon, life_value=100, money=15000):
         self.name = name
@@ -23,3 +23,20 @@ class Role(object):
 
 r1 = Role('Alex', 'police', 'AK47') #生成一个角色
 r2 = Role('Jack', 'terrorist', 'B22')  #生成一个角色
+"""
+
+class Role(object):
+    def __init__(self,name,role,weapon,lift_value):
+        self.name = name
+        self.role = role
+        self.weapon = weapon
+        self.lift_value = lift_value
+
+    def buy_weapon(self,weapon):
+        print("%s is buying [%s]" %(self.name,weapon))
+
+# 实例化
+p1 = Role("SanJiang",'Police',"B11",90)
+t2 = Role("xie",'Terrorist',"B10",100)
+p1.buy_weapon("AK47")
+t2.buy_weapon("B11")
